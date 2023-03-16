2f851918dbd65ad66bcd47a26910d4393ff736b5
Tasneem Patrawala <tasneemp_sa@yahoo.com>
2023-03-16 14:50:55 -0400

    Add plaid keys to .env

---
+++ src/components/Budget/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Budget.css'
+import AreaChart from "../AreaChart";
+
+
+const Budget = () => {
+  return (
+    <div class="container py-4 px-3 mx-auto">
+      <AreaChart />
+    </div>
+  );
+};
+
+export default Budget;
