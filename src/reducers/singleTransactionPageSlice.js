b26eabdb1aa12fa24a3b02c064bfae074c3f8086
Chance Hemerda <cshemerda@gmail.com>
2023-03-16 08:27:10 -0700

    store made - slices added

---
+++ src/reducers/singleTransactionPageSlice.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+import { createSlice } from "@reduxjs/toolkit";
+
+export const singleTransactionPageSlice = createSlice({
+  name: "homePage",
+  initialState: {},
+  reducers: {},
+  extraReducers: (builder) => {},
+});
+
+export default singleTransactionPageSlice.reducer;
