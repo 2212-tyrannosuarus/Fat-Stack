ed1c37f8d8e0d307eb8eb5fd04781034f3d0e3bd
Chance Hemerda <cshemerda@gmail.com>
2023-03-17 09:46:12 -0700

    added footer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from "react";
 import Routes from "./Routes";
-// Import our custom CSS
 import "./scss/styles.scss";
-
-// Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
-      <div className="container py-4 px-3 mx-auto">
-        <h1 className="text-bg-dark">Hello, Bootstrap and Webpack!</h1>
-        <button className="btn btn-primary">Primary button</button>
-      </div>
       <Routes />
+      <Footer />
     </>
   );
 };
