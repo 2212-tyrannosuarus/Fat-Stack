1004cbdc411d08f9fb5c51ad61a0f21b1f67b72b
Tasneem Patrawala <tasneemp_sa@yahoo.com>
2023-03-17 17:46:50 -0400

    Add budget items and schemes

--- src/reducers/budgetPageSlice.js
+++ src/reducers/budgetPageSlice.js
@@ -1,57 +1,15 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const readTransactionCsv = createAsyncThunk(
-  "transactions/readCsv",
-  async ({ userId }) => {
-    const res = await fetch("/Tranactions_Mar_15.csv");
-    const resp = await res.text();
-    // console.log("resp", resp);
-    const csvData = resp.split("\n").map((row) => {
-      const [
-        date,
-        description,
-        amount,
-        tranactionType,
-        subCategory,
-        accountName,
-      ] = row.split(",");
-      return {
-        date: date.toString(),
-        description: description,
-        amount: parseFloat((amount * 1).toFixed(2)),
-        tranactionType: tranactionType,
-        subCategory: subCategory,
-        accountName: accountName,
-      };
-    });
-    console.log("cdata", csvData);
-    let csvData1 = [];
-    for (let i = 0; i <= 499; i++) {
-      csvData1.push(csvData[i]);
-    }
-
-    let csvData2 = [];
-    for (let i = 500; i <= 1020; i++) {
-      csvData2.push(csvData[i]);
-    }
-    let transactions = csvData1;
-    const { data1 } = await axios.post(`/api/budget/${userId}`, transactions);
-
-    transactions = csvData2;
-    const { data2 } = await axios.post(`/api/budget/${userId}`, transactions);
-
-    let resultData = [];
-    for (let i = 0; i < data1; i++) {
-      resultData.push(data1[i]);
-    }
-
-    for (let j = 0; j < data2; j++) {
-      resultData.push(data2[j]);
-    }
-    return resultData;
+export const fetchAllUserTransactions = createAsyncThunk(
+  "courses_sub_category/fetchAll",
+  async (userId) => {
+    userId = parseInt(userId);
+    const { data } = await axios.get(`/api/subCategories/${userId}`);
+    return data;
   }
 );
+
 
 export const budgetPageSlice = createSlice({
   name: "BudgetPage",
@@ -60,9 +18,10 @@ export const budgetPageSlice = createSlice({
   },
   reducers: {},
   extraReducers: (build) => {
-    build.addCase(readTransactionCsv.fulfilled, (state, action) => {
+    build
+    .addCase(fetchAllUserTransactions.fulfilled, (state, action) => {
       state.transactions = action.payload;
-    });
+    })
   },
 });
 
