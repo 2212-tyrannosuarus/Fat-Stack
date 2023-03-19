96c0526054293f0b5bb39168d08fe5207fe3ea1f
miro <miro.maleb@gmail.com>
2023-03-19 17:49:59 -0400

    added thunks for getting bank account info to transactions reducers. displaying basic account info

--- src/components/AllTransactions/index.js
+++ src/components/AllTransactions/index.js
@@ -2,19 +2,37 @@ import React, { useEffect } from "react";
 import {
   fetchAllTransactions,
   selectAllTransactions,
+  selectAllBankAccounts,
+  fetchAllBankAccounts,
 } from "../../reducers/allTransactionsPageSlice";
 import { useDispatch, useSelector } from "react-redux";
 
 const AllTransactions = () => {
   const dispatch = useDispatch();
   const allTransactions = useSelector(selectAllTransactions);
+  const bankAccounts = useSelector(selectAllBankAccounts);
 
   useEffect(() => {
     dispatch(fetchAllTransactions());
+    dispatch(fetchAllBankAccounts());
   }, [dispatch]);
 
   return (
     <>
+      <div>
+        Your accounts:
+        <ul>
+          {bankAccounts.map((account) => {
+            return (
+              <li>
+                {account.account_name}|{account.available_balance}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div>Transactions:</div>
       <div>
         Date............Description............Category............Amount
       </div>
--- src/reducers/allTransactionsPageSlice.js
+++ src/reducers/allTransactionsPageSlice.js
@@ -9,21 +9,36 @@ export const fetchAllTransactions = createAsyncThunk(
   }
 );
 
+export const fetchAllBankAccounts = createAsyncThunk(
+  "bankAccounts/fetchAll",
+  async () => {
+    const { data } = await axios.get("/api/bankAccounts");
+    return data;
+  }
+);
+
 export const allTransactionsPageSlice = createSlice({
   name: "allTransactions",
   initialState: {
     allTransactions: [],
+    bankAccounts: [],
   },
   reducers: {},
   extraReducers: (builder) => {
     builder.addCase(fetchAllTransactions.fulfilled, (state, action) => {
       state.allTransactions = action.payload;
     });
+    builder.addCase(fetchAllBankAccounts.fulfilled, (state, action) => {
+      state.bankAccounts = action.payload;
+    });
   },
 });
 
 export const selectAllTransactions = (state) => {
   return state.allTransactionsPage.allTransactions;
+};
+export const selectAllBankAccounts = (state) => {
+  return state.allTransactionsPage.bankAccounts;
 };
 
 export default allTransactionsPageSlice.reducer;
