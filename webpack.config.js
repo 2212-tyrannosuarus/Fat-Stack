8ca6dcb306dc79add087df864af4ded91070e676
Tasneem Patrawala <tasneemp_sa@yahoo.com>
2023-03-20 12:49:28 -0400

    Add bootstrap loaders  into config file

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,30 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  require('autoprefixer')
+                ]
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
     ],
   },
 };
