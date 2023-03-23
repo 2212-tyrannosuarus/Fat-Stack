a0a55e60492f1debfbca0b924aa7386f66c3d4a8
Tasneem Patrawala <tasneemp_sa@yahoo.com>
2023-03-23 16:13:32 -0400

    Add styling to charts

--- src/components/BarChartBySubcategory/index.js
+++ src/components/BarChartBySubcategory/index.js
@@ -96,13 +96,69 @@ const BarChartBySubcategory = (props) => {
 
   return (
     <div className="container">
-      {categoriesArr && categoriesArr.length ? (
+      
+
+      {chartData &&
+      chartData.length &&
+      chartData.flat().slice(0, -1)[0] !== undefined &&
+      barChartDebit ? (
+        <div className="row">
+          <div className="col-8 p-0">
+        <VictoryChart
+          domainPadding={50}
+          padding={{ top: 200, bottom: 100, left: 100, right: 5 }}
+          height={700}
+          width={700}
+        >
+          <VictoryLegend
+            x={200}
+            y={60}
+            orientation="horizontal"
+            gutter={50}
+            colorScale={["#ff7960"]}
+            data={[
+              { name: `${chartData.flat().slice(0, -1)[0].subcategoryName}` },
+            ]}
+            style={{ labels: { fontSize: 25 } }}
+          />
+
+          <VictoryAxis
+            label={` Last 6 months `}
+            style={{
+              tickLabels: { fontSize: 25, padding: 5 },
+              axisLabel: { fontSize: 30, padding: 50 },
+            }}
+          />
+          <VictoryAxis
+            dependentAxis
+            // tickFormat specifies how ticks should be displayed
+            tickFormat={(x) => `$${x}`}
+            style={{ tickLabels: { fontSize: 25, padding: 5 } }}
+          />
+
+          <VictoryGroup offset={25}>
+            <VictoryBar
+              color="#ff7960"
+              // animate={{ duration: 2000, onLoad: { duration: 4000 } }}
+              data={barChartDebit}
+              x="monthYear"
+              y="total"
+              style={{ tickLabels: { fontSize: 15, padding: 5 } }}
+              barRatio={0.8}
+            />
+          </VictoryGroup>
+        </VictoryChart>
+        </div>
+
+        <div className="col-4 mt-n " align="center">
+
+        {categoriesArr && categoriesArr.length ? (
         <>
-          <h4 className="mb-2 mt-2">Choose a sub category </h4>
+           <h4 class="fw-bold mb-2 mt-2">Choose a sub-category</h4>
           <Select
             name="sub-categories"
             id="subCategory"
-            onChange={(evt) => setSubCategoryName(evt.target.value)}
+            onChange={(evt) => {setSubCategoryName(evt.target.value)}}
           >
             {categoriesArr && categoriesArr.length
               ? categoriesArr.map((category, index1) => {
@@ -136,9 +192,9 @@ const BarChartBySubcategory = (props) => {
               : "Loading sub categories"}
           </Select>
           <Button
-            variant="light"
+            variant="outline-dark"
             onClick={() => handleOvertimeSubcategory(subCategoryName)}
-            className="col-12 display-chart"
+            className="col-12 display-chart mt-3"
           >
             {" "}
             Display Chart
@@ -147,54 +203,8 @@ const BarChartBySubcategory = (props) => {
       ) : (
         "Loading subcategories"
       )}
-
-      {chartData &&
-      chartData.length &&
-      chartData.flat().slice(0, -1)[0] !== undefined &&
-      barChartDebit ? (
-        <VictoryChart
-          domainPadding={50}
-          padding={{ top: 125, bottom: 100, left: 100, right: 5 }}
-          height={600}
-          width={700}
-        >
-          <VictoryLegend
-            x={150}
-            y={30}
-            orientation="horizontal"
-            gutter={50}
-            colorScale={["#ff7960"]}
-            data={[
-              { name: `${chartData.flat().slice(0, -1)[0].subcategoryName}` },
-            ]}
-            style={{ labels: { fontSize: 25 } }}
-          />
-
-          <VictoryAxis
-            label={` Last 6 months `}
-            style={{
-              tickLabels: { fontSize: 25, padding: 5 },
-              axisLabel: { fontSize: 30, padding: 50 },
-            }}
-          />
-          <VictoryAxis
-            dependentAxis
-            // tickFormat specifies how ticks should be displayed
-            tickFormat={(x) => `$${x}`}
-            style={{ tickLabels: { fontSize: 25, padding: 5 } }}
-          />
-
-          <VictoryGroup offset={25}>
-            <VictoryBar
-              color="#ff7960"
-              // animate={{ duration: 2000, onLoad: { duration: 4000 } }}
-              data={barChartDebit}
-              x="monthYear"
-              y="total"
-              style={{ tickLabels: { fontSize: 15, padding: 5 } }}
-            />
-          </VictoryGroup>
-        </VictoryChart>
+          </div>
+        </div>
       ) : (
         "No data to diaplay"
       )}
